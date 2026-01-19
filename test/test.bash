4a31ae0e7c066325f1b7e68a864aefc2742040e7
Ruka Ishikawa <ishikawaruka.1225@gmail.com>
2026-01-19 21:32:23 +0900

    Fix GitHub Actions and test

--- test/test.bash
+++ test/test.bash
@@ -1,17 +1,23 @@
 #!/bin/bash
 set -e
 
-source ~/ros2_ws/install/setup.bash
+# ROS 2 環境
+source /opt/ros/humble/setup.bash
+source install/setup.bash
 
 # ノード起動
 ros2 run text_stats_node text_stats &
 NODEPID=$!
 
-# ノードが立ち上がるのを待つ
-sleep 3
+# 起動待ち
+sleep 2
 
-# /text_stats トピックが存在することを確認
-ros2 topic list | grep -q "/text_stats"
+# 入力を1回送信
+ros2 topic pub --once /input_text std_msgs/String "data: hello world"
 
+# 出力を1回だけ受信して検査
+ros2 topic echo /text_stats --once | grep -q "\[11, 2\]"
+
+# ノード停止
 kill $NODEPID
 
